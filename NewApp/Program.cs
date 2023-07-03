1f1d72d8ee67513b2d30afbbfac2c0c1f23e6372
DHung308 <nguyenduonghung30082001@gmail.com>
2023-07-03 22:05:53 +0700

    BTVN

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -5,5 +5,5 @@ string hoTen = Console.ReadLine();
 Console.WriteLine("Nhập tuổi:");
 int tuoi = Convert.ToInt32(Console.ReadLine());
 
-string ketQua = string.Format("Tên bạn là {0}, {1} tuổi chẳng hạn", hoTen, tuoi);
+string ketQua = string.Format("Tên bạn là {0}, {1} tuổi", hoTen, tuoi);
 Console.WriteLine(ketQua);
