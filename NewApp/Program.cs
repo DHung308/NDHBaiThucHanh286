04dc7f4fe3492eaf3ff5c435b601a3e72ec02091
DHung308 <nguyenduonghung30082001@gmail.com>
2023-07-03 16:26:52 +0700

    Nhập tên tuổi

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Nhập tên
+Console.WriteLine("Nhập họ và tên:");
+Console.WriteLine("Nhập tuổi:");
+
+string hoTen = Console.ReadLine();
+string tuoi = Console.ReadLine();
+
+Console.WriteLine("Họ và tên của bạn là: " + hoTen);
+Console.WriteLine("Tuổi:" + tuoi);
